b12f4126a36fd4a1794d8467ca1f1f25a308ddfb
Zekraviax <18898897+Zekraviax@users.noreply.github.com>
2021-03-20 22:30:14 +1100

    Begun re-implementing attacks in BP

--- Source/Starmark/Starmark_GameMode.cpp
+++ Source/Starmark/Starmark_GameMode.cpp
@@ -6,9 +6,21 @@
 
 
 // ------------------------- Battle
-void AStarmark_GameMode::GameMode_SetTurnOrder_Implementation()
+void AStarmark_GameMode::GameMode_LaunchAttack_Implementation(ACharacter_Pathfinder* Attacker, ACharacter_Pathfinder* Defender)
 {
+	int CurrentDamage = 1;
 
+	CurrentDamage = FMath::CeilToInt(Attacker->AvatarData.BaseStats.Attack * Attacker->CurrentSelectedAttack.BasePower);
+	GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Red, FString::Printf(TEXT("Current Damage 1 = %d"), CurrentDamage));
+	CurrentDamage = FMath::CeilToInt(CurrentDamage / Defender->AvatarData.BaseStats.Defence);
+	GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Red, FString::Printf(TEXT("Current Damage 2 = %d"), CurrentDamage));
+	CurrentDamage = FMath::CeilToInt((Attacker->AvatarData.BaseStats.Power / 2) * CurrentDamage);
+	GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Red, FString::Printf(TEXT("Current Damage 3 = %d"), CurrentDamage));
+	CurrentDamage = FMath::CeilToInt(CurrentDamage / 8);
+	GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Red, FString::Printf(TEXT("Current Damage 4 = %d"), CurrentDamage));
+
+
+	GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Red, FString::Printf(TEXT("Final Damage  = %d"), CurrentDamage));
 }
 
 
