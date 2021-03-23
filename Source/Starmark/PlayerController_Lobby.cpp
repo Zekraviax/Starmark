fd1b1e88847078ae944416677a3aaeb5525b5a09
Zekraviax <18898897+Zekraviax@users.noreply.github.com>
2021-03-23 19:22:39 +1100

    Some ,miscellaneous BP clean-up and refactoring into code  /  The Server can now send people to the MainMenu after a battle ends again

--- Source/Starmark/PlayerController_Lobby.cpp
+++ Source/Starmark/PlayerController_Lobby.cpp
@@ -1,2 +1,20 @@
 #include "PlayerController_Lobby.h"
 
+#include "Widget_ServerHost.h"
+
+
+// ------------------------- Widgets
+void APlayerController_Lobby::AddLobbyMenuToScreen()
+{
+	if (!LobbyWidget_Reference->IsValidLowLevel() && LobbyWidget_Class->IsValidLowLevel() && IsLocalPlayerController()) {
+		// Must assign a local controller
+		LobbyWidget_Reference = CreateWidget<UWidget_ServerHost>(this, LobbyWidget_Class);
+	}
+
+	if (LobbyWidget_Reference->IsValidLowLevel()) {
+		LobbyWidget_Reference->AddToViewport();
+	}
+
+	SetInputMode(FInputModeGameAndUI());
+	bShowMouseCursor = true;
+}
--- Source/Starmark/PlayerController_Lobby.h
+++ Source/Starmark/PlayerController_Lobby.h
@@ -4,10 +4,30 @@
 #include "GameFramework/PlayerController.h"
 #include "PlayerController_Lobby.generated.h"
 
+// Forward Declarations
+class UWidget_ServerHost;
+
 
 UCLASS()
 class STARMARK_API APlayerController_Lobby : public APlayerController
 {
 	GENERATED_BODY()
-	
+
+public:
+// Variables
+// --------------------------------------------------
+
+// ------------------------- Widgets
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSubclassOf<UWidget_ServerHost> LobbyWidget_Class;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UWidget_ServerHost* LobbyWidget_Reference;
+
+// Functions
+// --------------------------------------------------
+
+// ------------------------- Widgets
+	UFUNCTION(BlueprintCallable)
+	void AddLobbyMenuToScreen();
 };
